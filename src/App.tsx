d678e471485e144f81468743e14a155dc71c98de
Romain Coupey <schaft115@hotmail.com>
2022-04-18 21:12:59 +0200

    basic layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,29 @@
+import { AppBar, Container, CssBaseline, Stack, Toolbar, Typography } from '@mui/material';
+
+import img from './assets/logo.png';
+
 function App() {
-  return <h1>Hello World</h1>;
+	return (
+		<>
+			<CssBaseline />
+			<AppBar position="static" sx={{ backgroundColor: 'background.paper', padding: 1, mb: 8 }}>
+				<Toolbar>
+					<Container>
+						<img width="247" height="35" src={img} alt="In The Memory logo" loading="lazy" />
+					</Container>
+				</Toolbar>
+			</AppBar>
+			<main>
+				<Container>
+					<Stack spacing={6}>
+						<Typography variant="h3">Country</Typography>
+						<Typography variant="h3">Summary</Typography>
+						<Typography variant="h3">Revenue per month</Typography>
+					</Stack>
+				</Container>
+			</main>
+		</>
+	);
 }
 
 export default App;
